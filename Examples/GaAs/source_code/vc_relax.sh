44b57722cab94297a82921a09ec313810a87ad0b
Mohan Giri <87503305+mohangiri1@users.noreply.github.com>
2024-07-16 21:41:06 -0500

    Create vc_relax.sh

---
+++ Examples/GaAs/source_code/vc_relax.sh
@@ -0,0 +1,66 @@
+#!/bin/bash 
+
+# Get the current working directory and change to it
+cd /data/girim/Research/GaAs/convergence/geometric_optimize
+
+# Load the Quantum ESPRESSO module
+module load qe/7.0
+
+ecutwfc=80
+ecut=$(($ecutwfc*9))
+k=8
+calculation='vc-relax'
+
+cat > vc-relax.in << EOF
+&CONTROL
+  calculation = $calculation
+  prefix = 'GaAs'
+  outdir = './output/'
+  pseudo_dir = '/data/girim/Research/pseudopotential/LDA/'
+  disk_io = 'none'
+/
+
+&SYSTEM
+  degauss = 0.002
+  ecutrho = $ecut
+  ecutwfc = $ecutwfc
+  ibrav = 2
+  celldm(1) = 10.7073764239
+  lspinorb = .TRUE.
+  nat = 2
+  noncolin = .TRUE.
+  ntyp = 2
+  occupations = 'smearing'
+  smearing = 'gaussian'
+  starting_magnetization(1) = 0.0000e+00
+  nosym = .true.
+/
+
+&ELECTRONS
+  conv_thr = 1e-08
+  electron_maxstep = 80
+  mixing_beta = 0.3
+/
+
+&ions
+	ion_dynamics = 'bfgs'
+/
+
+&cell
+    cell_dofree='ibrav'
+    cell_dynamics = 'bfgs'
+/
+ATOMIC_SPECIES
+  As 74.9216 As.rel-pz-n-rrkjus_psl.0.2.UPF
+  Ga 69.72 Ga.rel-pz-dn-rrkjus_psl.0.2.UPF
+
+K_POINTS {automatic}
+  $k $k $k 0 0 0
+
+ATOMIC_POSITIONS (crystal)
+  Ga 0.0   0.0   0.0
+  As 0.25  0.25  0.25
+EOF
+
+# Run SCF calculation.
+mpirun -np 32 pw.x < vc-relax.in > vc-relax.out
