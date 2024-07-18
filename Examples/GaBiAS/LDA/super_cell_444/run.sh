7879d1b54e74dce608e689cb8a315cf84c2dfade
Mohan Giri <87503305+mohangiri1@users.noreply.github.com>
2024-07-18 13:57:05 -0500

    Update run.sh

--- Examples/GaBiAS/LDA/super_cell_444/run.sh
+++ Examples/GaBiAS/LDA/super_cell_444/run.sh
@@ -4,10 +4,10 @@
 cd /data/girim/GaBiAs/LDA/super_cell_444/
 
 # Activate the Python virtual environment
-source /home/girim/python_venv/my-python/bin/activate
+#source /home/girim/python_venv/my-python/bin/activate
 
 # Run the script to generate k-points
-python3 generate_kpoints.py
+#python3 generate_kpoints.py
 
 # Load the Quantum ESPRESSO module
 module load qe/7.0
@@ -18,8 +18,59 @@ PREFIX=GaBiAs
 # Run the SCF calculation in parallel using 16 processors
 mpirun -np 32 pw.x < "${PREFIX}-scf.in" > "${PREFIX}-scf.out"
 
+#Create a script for sending the email
+cat > email_result.sh <<'EOF'
+#!/bin/bash
+#$ -N SendEmail
+#$ -m bea
+#$ -M user@domain.com
+
+# Variables
+recipient="mohan_giri1@baylor.edu"
+subject="Completion of the Run"
+body="The job in the HPC has been completed. The results are attached in the compressed file."
+attachment="GaBiAs-scf.out"
+
+# Send email
+echo -e "$body" | /usr/bin/mail -s "$subject" -a "$attachment" "$recipient"
+
+echo "Email has been sent to Mohan."
+EOF
+
+# make executable
+
+chmod +x email_result.sh
+#run the executable
+./email_result.sh
+
+
 # Run the bands calculation in parallel using 16 processors
 mpirun -np 32 pw.x < "${PREFIX}-bands.in" > "${PREFIX}-bands.out"
+
+#Create a script for sending the email
+cat > email_result.sh <<'EOF'
+#!/bin/bash
+#$ -N SendEmail
+#$ -m bea
+#$ -M user@domain.com
+
+# Variables
+recipient="mohan_giri1@baylor.edu"
+subject="Completion of the Run"
+body="The job in the HPC has been completed. The results are attached in the compressed file."
+attachment="GaBiAs-bands.out"
+
+# Send email
+echo -e "$body" | /usr/bin/mail -s "$subject" -a "$attachment" "$recipient"
+
+echo "Email has been sent to Mohan."
+EOF
+
+# make executable
+
+chmod +x email_result.sh
+#run the executable
+./email_result.sh
 
 # Activate the Python virtual environment
 source /home/girim/python_venv/my-python/bin/activate
